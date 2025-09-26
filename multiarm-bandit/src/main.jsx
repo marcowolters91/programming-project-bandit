1748f40f0ec7f48fe20be7f4ba04b1c79eee2047
fheitlag <flx.htlg@gmail.com>
2025-09-26 16:32:16 +0200

    Ordnerstruktur + Seiten geändert

--- multiarm-bandit/src/main.jsx
+++ multiarm-bandit/src/main.jsx
@@ -1,20 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import App from './App.jsx'
-import BernoulliBandit_App from './BernoulliBandit.jsx'
-import Methode2 from './Methode2.jsx'
-import './App.css'
+import './app/BanditApp.css'
+import BanditApp from './app/BanditApp.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/bernoulli-bandit" element={<BernoulliBandit_App />} />
-        <Route path="/methode-2" element={<Methode2 />} />
-        <Route path="*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <BanditApp />
   </React.StrictMode>
 )
