a23c1c89d55eec9bda63e40fca948224c394310d
Marco Wolters <marcowolters2001@aol.com>
2025-10-05 18:40:27 +0200

    SCRUM-33 Update ESLint configuration to ignore 'coverage' directory

--- multiarm-bandit/eslint.config.js
+++ multiarm-bandit/eslint.config.js
@@ -7,7 +7,7 @@ import eslintPluginPrettier from 'eslint-plugin-prettier';
 import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'coverage']),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
