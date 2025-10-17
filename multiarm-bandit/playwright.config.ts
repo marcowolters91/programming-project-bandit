c3686967567fccacc98bf54fffd0b58c090c6bab
Marco Wolters <marcowolters2001@aol.com>
2025-10-17 13:31:48 +0200

    SCRUM-119 Add Chrome configuration for Playwright setup

--- multiarm-bandit/playwright.config.ts
+++ multiarm-bandit/playwright.config.ts
@@ -27,9 +27,13 @@ export default defineConfig({
     //   name: 'chromium',
     //   use: { ...devices['Desktop Chrome'] },
     // },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
+        {
+      name: 'chrome',
+      use: { ...devices['Desktop Chrome'] },
     },
     // {
     //   name: 'webkit',
