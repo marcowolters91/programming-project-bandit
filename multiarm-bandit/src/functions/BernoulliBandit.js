21b275416c3732e83c935b1890e967656c3767db
fheitlag <flx.htlg@gmail.com>
2025-10-17 11:35:46 +0200

    Auslagerung der Funktionen + Einbindung in BernoulliBandit.jsx

--- multiarm-bandit/src/functions/BernoulliBandit.js
+++ multiarm-bandit/src/functions/BernoulliBandit.js
@@ -1,15 +1,30 @@
 export default class BernoulliBandit {
-  constructor(probabilities) {
-    this.probabilities = probabilities;
+  constructor(armNames) {
+    this.armNames = [...armNames];
+    this.K = this.armNames.length;
+
+    this.probabilities = Array.from({ length: this.K }, () =>
+      Math.round((Math.random() * 0.9 + 0.05) * 100) / 100
+    );
   }
+
   pull(armIndex) {
-    if (armIndex < 0 || armIndex >= this.probabilities.length) {
-      throw new Error('Ungültiger Arm gewählt');
+    if (armIndex < 0 || armIndex >= this.K) {
+      throw new Error('Ungültiger Arm-Index im BernoulliBandit');
     }
     const p = this.probabilities[armIndex];
     return Math.random() < p ? 1 : 0;
   }
+
   getArms() {
-    return this.probabilities.length;
+    return this.K;
+  }
+
+  getProbabilities() {
+    return [...this.probabilities];
+  }
+
+  getArmNames() {
+    return [...this.armNames];
   }
 }
