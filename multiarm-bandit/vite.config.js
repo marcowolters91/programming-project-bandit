af86847b8e5d4db92c7ee7841f8781c028e186d4
Marco Wolters <marcowolters2001@aol.com>
2025-10-18 18:02:01 +0200

    SCRUM-121 Add build configuration for chunk size warning limit in vite.config.js

--- multiarm-bandit/vite.config.js
+++ multiarm-bandit/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/programming-project-bandit/',
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
 });
